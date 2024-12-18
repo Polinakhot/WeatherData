f6cdaf8fb8c30186136a6c6f5aa3938b4e80841d
Polinakhot <polinak9@nycstudents.net>
2024-12-18 14:31:57 +0000

    test cases

---
+++ WeatherData.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class WeatherData
+{
+   
+    private ArrayList<Double> temperatures;
+
+    public String toString()
+    {
+        return temperatures.toString();
+    }
+
+    public WeatherData(ArrayList<Double> temps)
+    {
+        temperatures = temps;
+    }
+
+
+
+    public void cleanData(double lower, double upper)
+    {
+        for(int i = 0; i < temperatures.size(); i++)
+        {
+            double temp = temperatures.get(i);
+            if(temp < lower || temp > upper)
+            {
+                temperatures.remove(i);
+                i--;
+            }
+        }
+    }
+
+   
+    public int longestHeatWave(double threshold)
+    {
+        int heatWave = 0;
+        int max = 0;
+        for(double t : temperatures)
+        {
+            heatWave ++;
+        }
+        if(heatWave > max) max = heatWave;
+            else heatWave = 0;
+            return max;
+    }
+    
+
+    
+
+}
